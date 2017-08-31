7b64fd848beca0ff4efeb472a477c645a7f07e95
rcd27 <redtom@yandex.ru>
2017-08-31 20:13:31 +0300

    master: registered Magnetto application to Instagram API.

---
+++ app/src/main/java/com/githib/rcd27/instagrallax/MagnettoApp.java
@@ -0,0 +1,8 @@
+package com.githib.rcd27.instagrallax;
+
+
+import android.app.Application;
+
+public class MagnettoApp extends Application {
+    private static final String CLIENT_ID = "52fc2ff703784e9fb6e65f1baa7b63f8";
+}
