45adfce693c698130ed47b2f72e756898f71fc8d
rcd27 <redtom@yandex.ru>
2017-08-31 12:23:27 +0300

    Initial commit.

---
+++ app/src/main/java/com/githib/rcd27/instagrallax/MainActivity.java
@@ -0,0 +1,13 @@
+package com.githib.rcd27.instagrallax;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
