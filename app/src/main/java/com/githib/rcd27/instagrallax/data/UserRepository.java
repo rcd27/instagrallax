f5d2e85f168a94953c9d12f7c1d130fcbe18c273
rcd27 <redtom@yandex.ru>
2017-09-03 14:45:15 +0300

    ARCH#3: devided user feature in two layers: view and presenter. UserRepository has no longer static fields or methods.

--- app/src/main/java/com/githib/rcd27/instagrallax/data/UserRepository.java
+++ app/src/main/java/com/githib/rcd27/instagrallax/data/UserRepository.java
@@ -10,15 +10,14 @@ import io.reactivex.Observable;
 
 public class UserRepository {
 
-    private static final String[] SUGGESTIONS = {
-            "Иван Охлобыстин", "Андрей Кочергин", "Михаил Кокляев",
-            "Сергий Алиев", "Федор Емельяненко", "Эд Халилов",
-            "Сергей Бадюк", "Александр Невский", "Евгений Радионов", "Виталий Лебедь"
-    };
+    private final SparseArray<String> users = new SparseArray<>();
 
-    private static final SparseArray<String> users = new SparseArray<>();
-
-    static {
+    public UserRepository() {
+        String[] SUGGESTIONS = {
+                "Иван Охлобыстин", "Андрей Кочергин", "Михаил Кокляев",
+                "Сергий Алиев", "Федор Емельяненко", "Эд Халилов",
+                "Сергей Бадюк", "Александр Невский", "Евгений Радионов", "Виталий Лебедь"
+        };
         Random gimmeRandom = new Random();
         for (String s : SUGGESTIONS) {
             int randomID = Math.abs(gimmeRandom.nextInt());
@@ -26,15 +25,15 @@ public class UserRepository {
         }
     }
 
-    public static Observable<SparseArray<String>> getSuggestions(@NonNull String forQuery) {
+    public Observable<SparseArray<String>> getSuggestions(@NonNull String forQuery) {
         return Observable.just(users);
     }
 
-    public static Observable<SparseArray<String>> getAllSuggestions() {
+    public Observable<SparseArray<String>> getAllSuggestions() {
         return Observable.just(users);
     }
 
-    public static Observable<String> getUserById(int id) {
+    public Observable<String> getUserById(int id) {
         String result = users.get(id);
         if (null != result) {
             return Observable.just(users.get(id));
