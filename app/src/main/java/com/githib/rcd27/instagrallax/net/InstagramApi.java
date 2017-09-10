a94660cfc6184335ff7472b1498dd98a1837fe34
rcd27 <redtom@yandex.ru>
2017-09-10 13:05:02 +0300

    master: this is not an error any more, but for consistence.

--- app/src/main/java/com/githib/rcd27/instagrallax/net/InstagramApi.java
+++ app/src/main/java/com/githib/rcd27/instagrallax/net/InstagramApi.java
@@ -8,15 +8,15 @@ import retrofit2.http.Query;
 
 public interface InstagramApi {
 
-    @GET("v1/users/search")
+    @GET("/v1/users/search")
     Observable<SearchResultDTO> searchUser(@Query("q") String searchQuery);
 
     @GET("/v1/users/{user-id}/media/recent")
     Observable<UserRecentMediaDTO> getRecentMediaForUser(@Path("user-id") long userId);
 
-    @GET("v1/users/{user-id}/")
+    @GET("/v1/users/{user-id}/")
     Observable<UserDataByIdDTO> getUserDataById(@Path("user-id") long userId);
 
-    @GET("v1/media/{media-id}/comments/")
+    @GET("/v1/media/{media-id}/comments/")
     Observable<MediaDataDTO> getCommentsForPost(@Path("media-id") String postId);
 }
