75bcfdb3fc8387362714248fcc20c9531bace750
rcd27 <redtom@yandex.ru>
2017-09-12 20:46:02 +0300

    master: todos added.

--- app/src/main/java/com/githib/rcd27/instagrallax/MagnettoApp.java
+++ app/src/main/java/com/githib/rcd27/instagrallax/MagnettoApp.java
@@ -8,6 +8,13 @@ import com.githib.rcd27.instagrallax.dagger.ApplicationModule;
 import com.githib.rcd27.instagrallax.dagger.DaggerAppComponent;
 import com.githib.rcd27.instagrallax.net.NetworkModule;
 
+/**
+ * TODO:
+ * 1) BasePresenter(cd:CompositeDisposable) <- all presenters implement add(), onDetach()
+ * 2) BaseRepository(st:SchedulerTranformer) <- all repos
+ * 3) PostImage: method refactor -> sort(String quality){...}
+ * 4) On image click: transfer @ResId instead of ImageView to the presenter
+ */
 public class MagnettoApp extends Application {
     public static final String BASE_URL = "https://api.instagram.com";
     public static final String ACCESS_TOKEN = "5968608397.efb6703.e766cc20812c4842ab903edded1148c9";
