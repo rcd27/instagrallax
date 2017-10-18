bda4bb88ee0da9b6b4ad904e75fd6f6bffbeded1
Земляков Станислав  / Zemlyakov Stanislav <redtom@yandex.ru>
2017-10-18 15:01:42 +0300

    Update ApplicationModule.java

--- app/src/main/java/com/github/rcd27/instagrallax/dagger/ApplicationModule.java
+++ app/src/main/java/com/github/rcd27/instagrallax/dagger/ApplicationModule.java
@@ -11,6 +11,8 @@ public class ApplicationModule {
 
     private final Context context;
 
+    // FIXME: то есть для всего приложения подготавливается ApplicationContext? Если да, то пофиксить на использование конкретного
+    // контекста для конкретных нужд.
     public ApplicationModule(Context context) {
         this.context = context;
     }
